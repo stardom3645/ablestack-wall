0d3aad487385efeddc90f8b3dd3889efaa14e883
stardom3645 <stardom3645@gmail.com>
2025-03-07 11:19:35 +0900

    오류 수정

--- public/app/features/dashboard/state/initDashboard.ts
+++ public/app/features/dashboard/state/initDashboard.ts
@@ -19,15 +19,7 @@ import { getFolderByUid } from 'app/features/folders/state/actions';
 import { dashboardWatcher } from 'app/features/live/dashboard/dashboardWatcher';
 import { playlistSrv } from 'app/features/playlist/PlaylistSrv';
 import { toStateKey } from 'app/features/variables/utils';
-import {
-  DASHBOARD_FROM_LS_KEY,
-  DashboardDTO,
-  DashboardInitPhase,
-  DashboardRoutes,
-  StoreState,
-  ThunkDispatch,
-  ThunkResult,
-} from 'app/types';
+import { DashboardDTO, DashboardInitPhase, DashboardRoutes, StoreState, ThunkDispatch, ThunkResult } from 'app/types';
 
 import { createDashboardQueryRunner } from '../../query/state/DashboardQueryRunner/DashboardQueryRunner';
 import { initVariablesTransaction } from '../../variables/state/actions';
@@ -58,6 +50,13 @@ async function fetchDashboard(
   dispatch: ThunkDispatch,
   getState: () => StoreState
 ): Promise<DashboardDTO | null> {
+  // When creating new or adding panels to a dashboard from explore we load it from local storage
+  const model = store.getObject<DashboardDTO>(DASHBOARD_FROM_LS_KEY);
+  if (model) {
+    removeDashboardToFetchFromLocalStorage();
+    return model;
+  }
+
   try {
     switch (args.routeName) {
       case DashboardRoutes.Home: {
@@ -185,14 +184,13 @@ export function initDashboard(args: InitDashboardArgs): ThunkResult<void> {
 
     // fetch dashboard data
     const dashDTO = await fetchDashboard(args, dispatch, getState);
+
     const versionBeforeMigration = dashDTO?.dashboard?.version;
 
     // returns null if there was a redirect or error
     if (!dashDTO) {
       return;
     }
-
-    addPanelsFromLocalStorage(dashDTO);
 
     // set initializing state
     dispatch(dashboardInitServices());
@@ -300,18 +298,12 @@ export function initDashboard(args: InitDashboardArgs): ThunkResult<void> {
   };
 }
 
-function addPanelsFromLocalStorage(model: DashboardDTO) {
-  // When creating new or adding panels to a dashboard from explore we load it from local storage
-  const fromLS = store.getObject<DashboardDTO>(DASHBOARD_FROM_LS_KEY);
-  if (fromLS) {
-    if (fromLS.dashboard.panels) {
-      model.dashboard.panels = fromLS.dashboard.panels.concat(model.dashboard.panels);
-    }
+export const DASHBOARD_FROM_LS_KEY = 'DASHBOARD_FROM_LS_KEY';
 
-    if (fromLS.dashboard.time) {
-      model.dashboard.time = fromLS.dashboard.time;
-    }
+export function setDashboardToFetchFromLocalStorage(model: DashboardDTO) {
+  store.setObject(DASHBOARD_FROM_LS_KEY, model);
+}
 
-    store.delete(DASHBOARD_FROM_LS_KEY);
-  }
+export function removeDashboardToFetchFromLocalStorage() {
+  store.delete(DASHBOARD_FROM_LS_KEY);
 }
