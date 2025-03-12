9137885f34060d54f8c53d2b922a220bd7ee51ec
stardom3645 <stardom3645@gmail.com>
2025-03-12 15:03:50 +0900

    alert mail grafana 제거

--- public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithDataSource.tsx
+++ public/app/features/plugins/admin/components/GetStartedWithPlugin/GetStartedWithDataSource.tsx
@@ -5,7 +5,8 @@ import { DataSourcePluginMeta } from '@grafana/data';
 import { config } from '@grafana/runtime';
 import { Button } from '@grafana/ui';
 import { t } from 'app/core/internationalization';
-import { useDataSourcesRoutes, addDataSource } from 'app/features/datasources/state';
+import { ROUTES } from 'app/features/connections/constants';
+import { addDataSource } from 'app/features/datasources/state';
 import { useDispatch } from 'app/types';
 
 import { isDataSourceEditor } from '../../permissions';
@@ -17,22 +18,20 @@ type Props = {
 
 export function GetStartedWithDataSource({ plugin }: Props): React.ReactElement | null {
   const dispatch = useDispatch();
-  const dataSourcesRoutes = useDataSourcesRoutes();
   const onAddDataSource = useCallback(() => {
     const meta = {
       name: plugin.name,
       id: plugin.id,
     } as DataSourcePluginMeta;
 
-    dispatch(addDataSource(meta, dataSourcesRoutes.Edit));
-  }, [dispatch, plugin, dataSourcesRoutes]);
+    dispatch(addDataSource(meta, ROUTES.DataSourcesEdit));
+  }, [dispatch, plugin]);
 
   if (!isDataSourceEditor()) {
     return null;
   }
 
-  const disabledButton =
-    config.featureToggles.managedPluginsInstall && config.pluginAdminExternalManageEnabled && !plugin.isFullyInstalled;
+  const disabledButton = config.pluginAdminExternalManageEnabled && !plugin.isFullyInstalled;
 
   return (
     <Button
